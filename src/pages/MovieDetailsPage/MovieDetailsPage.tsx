4b6031a7457cdaaaab498981019d4c68b29089fa
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-08 13:15:31 +0530

    FEATURE: update MovieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,5 +1,42 @@
-const MovieDetailsPage = () => {
-  return <div className="text-white">MovieDetailsPage</div>;
+import { useParams } from "react-router-dom";
+import { MovieDetailsShowcase } from "../../components/MovieDetailsShowcase";
+import { MovieData } from "../../data/movieData";
+import type { MovieDetails } from "../../utils/interface";
+import { MovieDetailsDescription } from "../../components/MovieDetailsDescription";
+
+const MovieDetails = () => {
+  const { movieId } = useParams();
+
+  const movie: MovieDetails | undefined = MovieData.find(
+    (item) => item.id.toString() === movieId
+  );
+
+  return (
+    <div
+      className="h-[calc(100vh-5.75rem)] overflow-y-scroll pt-5"
+      style={{ scrollbarWidth: "none" }}
+    >
+      {movie ? (
+        <div>
+          <MovieDetailsShowcase
+            id={movie.id}
+            title={movie?.title}
+            backPosterUrl={movie.backPosterUrl}
+            genre={movie.genre}
+            votes={movie.votes}
+          />
+          <MovieDetailsDescription
+            key={movie.id}
+            description={movie.description}
+            cast={movie.cast}
+            director={movie.director}
+          />
+        </div>
+      ) : (
+        <p className="text-white">Movie data is not found</p>
+      )}
+    </div>
+  );
 };
 
-export default MovieDetailsPage;
+export default MovieDetails;
