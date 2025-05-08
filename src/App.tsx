deb885889c360862b14e463c657b4074ddcd941f
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-08 11:44:40 +0530

    FEATURE: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
 import MainFeed from "./pages/MainFeedPage/MainFeedPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainFeed />,
+  },
+  {
+    path: "/details/:movieId",
+    element: <MovieDetailsPage />,
+  },
+]);
 
 function App() {
   return (
     <div>
       <Header />
-      <MainFeed />
+      <RouterProvider router={router} />
     </div>
   );
 }
