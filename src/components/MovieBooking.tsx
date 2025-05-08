84055d2561f1cc4a789f279a39c1ac55dd137b81
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-08 14:40:12 +0530

    FEATURE: create MovieBooking component

---
+++ src/components/MovieBooking.tsx
@@ -0,0 +1,21 @@
+import type { MovieDetails } from "../utils/interface";
+import { MovieBookingSeats } from "./ui/MovieBookingSeats";
+import { MovieTiming } from "./ui/MovieTiming";
+
+export const MovieBook = ({
+  title,
+  genre,
+}: Pick<MovieDetails, "title" | "genre">) => {
+  return (
+    <div className="pt-5">
+      <div className="max-w-[70%] mx-auto space-y-5">
+        <p className="text-amber-200 text-4xl font-semibold">{title}</p>
+        <span className="text-white text-xl border border-[var(--primary-color)] px-4 py-2 rounded-full">
+          {genre}
+        </span>
+      </div>
+      <MovieTiming />
+      <MovieBookingSeats />
+    </div>
+  );
+};
