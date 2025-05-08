33f8e809f483cd80cba805a0d9fedb6ca08fe7e0
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-08 15:27:07 +0530

    FEATURE: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import MainFeed from "./pages/MainFeedPage/MainFeedPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import MovieBooking from "./pages/MovieBookingPage/MovieBookingPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainFeed />,
-  },
-  {
-    path: "/details/:movieId",
-    element: <MovieDetailsPage />,
-  },
-  {
-    path: "/booking/:movieId",
-    element: <MovieBooking />,
-  },
-]);
 
 function App() {
   return (
     <div>
       <Header />
-      <RouterProvider router={router} />
+      <Outlet />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,35 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import MainFeed from "./pages/MainFeedPage/MainFeedPage.tsx";
+import { createBrowserRouter } from "react-router";
+import MovieDetails from "./pages/MovieDetailsPage/MovieDetailsPage.tsx";
+import MovieBooking from "./pages/MovieBookingPage/MovieBookingPage.tsx";
+import { RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <MainFeed />,
+      },
+      {
+        path: "/details/:movieId",
+        element: <MovieDetails />,
+      },
+      {
+        path: "/booking/:movieId",
+        element: <MovieBooking />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
