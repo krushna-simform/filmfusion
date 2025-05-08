4b00213f3afcb99e0d1e1ac0c7c2155c5bfba929
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-08 12:11:40 +0530

    FEATURE: update interface for movie details

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -1,27 +1,30 @@
-interface MovieCard {
-  posterUrl: string;
-  title: string;
-  description: string;
-  backPosterUrl: string;
-  positiveVotes: number;
-  negativeVotes: number;
-}
-
 type Votes = {
   total: number;
   positive: number;
   negative: number;
 };
 
+type Actor = {
+  actorName: string;
+  imageUrl: string;
+  role: string;
+};
+
+type Director = {
+  directorName: string;
+  imageUrl: string;
+};
+
 interface MovieDetails {
+  id: number;
   title: string;
   description: string;
   genre: string;
   posterUrl: string;
   backPosterUrl: string;
-  cast: Array<string>;
-  director: string | Array<string>;
+  cast: Array<Actor>;
+  director: Array<Director>;
   votes: Votes;
 }
 
-export type { MovieCard, MovieDetails };
+export type { MovieDetails, Votes, Actor, Director };
