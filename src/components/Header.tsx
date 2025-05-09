5c2b3513f2cd85a2be70a98ce6d0d7cd15a0c2b9
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-09 10:44:44 +0530

    FEATURE: update header element

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,9 @@ const Header = () => {
   return (
     <header className="w-[70%] mx-auto text-white flex justify-between py-6 items-center">
       <div>
-        <p
-          className="text-[var(--primary-color)] text-4xl font-bold"
-          role="heading"
-          aria-label="FilmFusion"
-        >
+        <h1 className="text-[var(--primary-color)] text-4xl font-bold">
           FilmFusion
-        </p>
+        </h1>
       </div>
       <div className="flex items-center gap-4">
         <Search />
