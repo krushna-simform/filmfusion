47e119dfd1bde22deb4364c06ae18a10e8825101
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-09 12:18:17 +0530

    FEATURE: create 404 error page

---
+++ src/components/ui/PageNotFound.tsx
@@ -0,0 +1,8 @@
+export const PageNotFound = () => {
+  return (
+    <div className="w-[70%] m-auto h-full flex flex-col items-center pt-70">
+      <p className="text-white text-9xl">404</p>
+      <p className="text-white text-2xl">Page not found</p>
+    </div>
+  );
+};
