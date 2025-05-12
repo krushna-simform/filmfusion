911fe1672a641d3e0f3fa2bbbfa4014e7f462355
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-12 12:04:41 +0530

    FEATURE: add Back to Home Page button into 404 page not found component

--- src/components/ui/PageNotFound.tsx
+++ src/components/ui/PageNotFound.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router";
+import { Button } from "./Button";
+
 export const PageNotFound = () => {
   return (
     <div className="w-[70%] m-auto h-full flex flex-col items-center pt-70">
       <p className="text-white text-9xl">404</p>
       <p className="text-white text-2xl">Page not found</p>
+      <Link to="/" className="mt-6">
+        <Button ariaLabel="Back to Home page" children="Back to Home page" />
+      </Link>
     </div>
   );
 };
