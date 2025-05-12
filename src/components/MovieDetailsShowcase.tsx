bed0d124f55893c4dfce0bace563692a36864de6
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-12 10:46:12 +0530

    FEATURE: add proper alignment to movieDetails page showcase section

--- src/components/MovieDetailsShowcase.tsx
+++ src/components/MovieDetailsShowcase.tsx
@@ -21,7 +21,7 @@ export const MovieDetailsShowcase = ({
   };
 
   return (
-    <div className="relative w-full h-[28rem] flex items-center text-white">
+    <div className="relative w-full md:h-[28rem] flex flex-col md:flex-row items-center text-white">
       <div
         className="absolute inset-0 bg-cover bg-right"
         style={{ backgroundImage: `url(${backPosterUrl})` }}
@@ -29,17 +29,20 @@ export const MovieDetailsShowcase = ({
 
       <div className="absolute inset-0 bg-gradient-to-r from-black via-black/90 to-transparent backdrop-blur-sm" />
 
-      <div className="relative z-10 flex w-full max-w-7xl mx-auto px-6 items-center space-x-8">
+      <div className="relative z-10 flex flex-col md:flex-row w-[90%] md:w-[70%] mx-auto items-center space-x-0 md:space-x-8">
         <DetailsPageMovieCard backPosterUrl={backPosterUrl} title={title} />
-        <div className="space-y-3 max-w-xl">
-          <h1 className="text-4xl font-bold">{title}</h1>
+        <div className="space-y-3 max-w-xl text-center md:text-left">
+          <h1 className="text-3xl md:text-4xl font-bold">{title}</h1>
           <Rating
             negative={votes.negative}
             positive={votes.positive}
             fontSize="2xl"
           />
-          <p className="text-xl text-gray-300">{genre}</p>
-          <div onClick={bookTicketButtonHandler}>
+          <p className="text-lg md:text-xl text-gray-300">{genre}</p>
+          <div
+            onClick={bookTicketButtonHandler}
+            className="flex justify-center md:justify-start"
+          >
             <Button children="Book Ticket" ariaLabel="Book Ticket" key={id} />
           </div>
         </div>
