159f348bfad8d93c03eec304f9eb83a2e3231a8b
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-14 11:52:50 +0530

    FEATURE: move MovieDescriptionProps type outside of the component

--- src/components/MovieDetailsDescription.tsx
+++ src/components/MovieDetailsDescription.tsx
@@ -3,11 +3,16 @@ import { AboutMovie } from "./ui/AboutMovie";
 import { Cast } from "./ui/Cast";
 import { Director } from "./ui/Director";
 
+type MovieDescriptionProps = Pick<
+  MovieDetails,
+  "description" | "cast" | "director"
+>;
+
 export const MovieDetailsDescription = ({
   description,
   cast,
   director,
-}: Pick<MovieDetails, "description" | "cast" | "director">) => {
+}: MovieDescriptionProps) => {
   return (
     <div className="w-[70%] m-auto">
       <AboutMovie description={description} />
