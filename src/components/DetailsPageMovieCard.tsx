bb190f3ce4263e94094eef9036cc649515d72e4a
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-14 11:34:31 +0530

    FEATURE: move card props type outside of the component

--- src/components/DetailsPageMovieCard.tsx
+++ src/components/DetailsPageMovieCard.tsx
@@ -1,10 +1,12 @@
 import type { MovieDetails } from "../types/interface";
 import { Image } from "./ui/Image";
 
+type MovieDetailsCardProps = Pick<MovieDetails, "title" | "backPosterUrl">;
+
 export const DetailsPageMovieCard = ({
   title,
   backPosterUrl,
-}: Pick<MovieDetails, "title" | "backPosterUrl">) => {
+}: MovieDetailsCardProps) => {
   return (
     <div className="min-w-64 h-100 rounded-2xl overflow-hidden" tabIndex={0}>
       <Image source={backPosterUrl} alt={`Poster of the movie ${title}`} />
